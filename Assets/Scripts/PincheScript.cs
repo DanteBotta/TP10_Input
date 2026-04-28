e746277afefa326a47e29533735b6d4fec224560
Damptermite4040 <208827267+DanteBotta@users.noreply.github.com>
2026-04-28 14:23:24 -0300

    Initial commit

---
+++ Assets/Scripts/PincheScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PincheScript : MonoBehaviour
+{
+    public float rotationSpeedY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rotationSpeedY = 3f;
+        }
+        if (Input.GetKeyUp(KeyCode.Space)){
+            rotationSpeedY = 0;
+        }
+        
+        transform.Rotate(0, rotationSpeedY, 0);
+    }
+}
